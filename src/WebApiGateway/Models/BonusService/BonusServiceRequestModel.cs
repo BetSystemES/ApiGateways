7a9d30b410fd0949d42a5243a0a6213a0b669048
Vlad Shamatulskiy <vlad.shamatulskiy@effective-soft.com>
2023-03-29 01:23:14 +0300

    Add cash controller get paged transaction history

--- src/WebApiGateway/Models/BonusService/BonusServiceRequestModel.cs
+++ src/WebApiGateway/Models/BonusService/BonusServiceRequestModel.cs
@@ -5,5 +5,6 @@ namespace WebApiGateway.Models.BonusService
     public class BonusServiceRequestModel : FilterCriteriaRequestModel
     {
         public string? ProfileId { get; set; }
+        public bool? IsEnabled { get; set; }
     }
 }
--- src/WebApiGateway/Models/Criterias/FilterCriteriaRequestModel.cs
+++ src/WebApiGateway/Models/Criterias/FilterCriteriaRequestModel.cs
@@ -3,7 +3,8 @@
     public class FilterCriteriaRequestModel : OrderCriteriaRequestModel
     {
         public List<Guid>? UserIds { get; set; }
-        public bool? IsEnabled { get; set; }
+        public DateTimeOffset? StartDate { get; set; }
+        public DateTimeOffset? EndDate { get; set; }
         public string? SearchCriteria { get; set; }
     }
 }
